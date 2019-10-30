afd182593f2cdb87b4e38816503c141f3d30299d
Pierlauro Sciarelli <pierlauro.sciarelli@bsc.es>
2019-10-30 18:42:56 +0100

    Test configuration

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import subprocess
 import tempfile
 
 from pathlib import Path
-from protobize import CompileProtoBuffers 
+from protobize import CompileProtoBuffers, ProtobizeConfiguration
 
 def test_find_protoc_local():
 	PATH = os.environ['PATH']
@@ -32,4 +32,32 @@ def test_download_protoc():
 	binary = cpb.download_protoc()
 	assert os.path.isfile(binary)
 	stdout = subprocess.getoutput(binary + ' --version')
-	assert stdout.startswith('libprotoc ') 
+	assert stdout.startswith('libprotoc ')
+
+def test_run():
+	cpb = CompileProtoBuffers()
+	cpb.run()
+
+def test_wrong_env_configuration():
+	""" Not existing configuration file """
+	try:
+		os.environ['protobize_conf'] = '__notexist__.xml'
+		pc = ProtobizeConfiguration()
+		assert len(pc.conf) == 0
+	finally:
+		os.environ['protobize_conf'] = ''
+
+def test_env_configuration():
+	""" Use provided xml file """
+	try:
+		os.environ['protobize_conf'] = 'protobize.xml'
+		pc = ProtobizeConfiguration()
+		assert len(pc.conf) > 0
+	finally:
+		os.environ['protobize_conf'] = ''
+
+def test_no_env_configuration():
+	""" Use default protobize.xml """
+	del os.environ['protobize_conf']
+	pc = ProtobizeConfiguration()
+	assert len(pc.conf) > 0
